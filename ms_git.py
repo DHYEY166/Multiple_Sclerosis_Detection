0f61a1c2028d96077cccbc5d97ba4d4a57838ae6
DHYEY166 <97232546+DHYEY166@users.noreply.github.com>
2024-06-20 20:48:47 +0530

    new

--- ms_git.py
+++ ms_git.py
@@ -39,7 +39,7 @@ st.write("4. MS-Sagittal")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert("RGB")  # Ensure image is in RGB mode
     st.image(image, caption='Uploaded Image', use_column_width=True)
     st.write("")
     st.write("Classifying...")
